6e84be3d8f81f743e86d7ba52a9398ba2c0b918f
daniel-giralt-len <daniel.giralt.len@gmail.com>
2023-04-27 19:50:18 +0200

    animate and save NChecks value

--- src/components/Check.tsx
+++ src/components/Check.tsx
@@ -3,10 +3,37 @@ const StyledCheck = styled.input`
     ${({isSquared}) => isSquared ? '' : ''}
 `
 
-export const Check = ({isSquared, disabled}) => {
+//${({aaa})=> aaa ? '' : ''}}
+export const Check = styled.div`
+    display: inline-block;
+
+    border: 1px solid black;
+    ${({isSquared})=> isSquared ? '' : 'border-radius: 1em;'}}
+
+    width: 1em;
+    height: 1em;
+
+    ${({isChecked})=> isChecked ? 'background: black;' : ''}}
+    ${({isHovered})=> isHovered ? 'background: blue;' : ''}}
+`
+
+export const Chheck = ({
+    isSquared,
+    disabled,
+    isHovered,
+    isChecked,
+    onMouseOver,
+    onMouseOut,
+    onClick,
+}) => {
     return (<StyledCheck
         disabled={disabled}
         isSquared={isSquared}
+        isHovered={isHovered}
+        isChecked={isChecked}
+        onMouseOver={onMouseOver}
+        onMouseOut={onMouseOut}
+        onClick={onClick}
         type='checkbox'
     />)
 }
--- src/components/NChecks.tsx
+++ src/components/NChecks.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react"
 import { Check } from "./Check"
+import styled from 'styled-components'
+
 export const NChecks = ({isSquared, length=5}) => {
-    
+    const [highlit, setHighlit] = useState({})
+    const [value, setValue] = useState(0)
+
+    const handleMouseOver = i => {
+        setHighlit({[i]: true})
+    }
+    const handleMouseOut = i => {
+        setHighlit({[i]: false})
+    }
     return (
         <div>
         {Array(length).fill().map((_,i)=>{
-            return (<Check key={i} isSquared={isSquared}/>)
+            const isHovered = highlit[i]
+            return (<Check
+                key={i}
+                isHovered={isHovered}
+                isChecked={value >= i+1}
+                onMouseOver={() => handleMouseOver(i)}
+                onMouseOut={() => handleMouseOut(i)}
+                onClick={() => setValue(i+1)}
+                isSquared={isSquared}
+            />)
         })}
         </div>
     )
