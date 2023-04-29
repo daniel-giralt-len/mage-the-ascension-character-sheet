149c7ce72c35d414073b97834127357449b1280f
daniel-giralt-len <daniel.giralt.len@gmail.com>
2023-04-29 15:04:05 +0200

    put type into every field

--- src/components/SkillRow.tsx
+++ src/components/SkillRow.tsx
@@ -1,18 +1,14 @@
 import { SectionRowHeader } from "./Headers"
 import { Skill } from "./Skill"
 
-export const SkillRow = ({title, fields, fieldsType}) => {
+export const SkillRow = ({title, fields}) => {
     const sortedFields = [...fields]
     sortedFields.sort()
 
     return (<div>
         <SectionRowHeader>{title}</SectionRowHeader>
-        {sortedFields.map((field,i)=>{
-            if(typeof field === 'string'){
-                return (<Skill name={field} type={fieldsType} key={i}/>)
-            }
-            return (<Skill name={field.name} type={field.type || fieldsType} table={field.table} key={i}/>)
-
+        {sortedFields.map(({name, type, table},i)=>{
+            return (<Skill name={name} type={type} table={table} key={i}/>)
         })}
     </div>)
 }
