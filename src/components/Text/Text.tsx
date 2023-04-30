52cb16b19de01eea98aee6044f3a74d57d2ea1da
daniel-giralt-len <daniel.giralt.len@gmail.com>
2023-04-30 20:29:53 +0200

    use react-hook-form for text and variableList

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { useFormContext } from 'react-hook-form'
+import { TextProps } from './types'
 
 const TextInput = styled.input `
     height: 1em;
@@ -6,6 +8,6 @@ const TextInput = styled.input `
     border-bottom: 1px solid black;
     max-width: 120px;
 `
-export const Text = () => (
-    <TextInput type='text'/>
+export const Text: React.FC<TextProps> = ({name}) => (
+    <TextInput type='text' {...useFormContext().register(name)} />
 )
--- src/components/VariableList/VariableList.tsx
+++ src/components/VariableList/VariableList.tsx
@@ -1,22 +1,24 @@
 import { useState } from "react"
 import { AddButton, FiveChecks, Text } from "../index"
 import styled from 'styled-components'
+import { VariableListProps } from "./types"
 
 const ListItem = styled.div`
     display: flex;
     justify-content: space-between;
 `
 
-export const VariableList = () => {
+export const VariableList: React.FC<VariableListProps
+> = ({name}) => {
     const [list, setList] = useState([{name:'',value:0}])
 
     const addNew = () => setList([...list, {name:'',value:0}])
     return (
         <div>
-            {list.map((item,i)=>(
+            {list.map((_,i)=>(
                 <ListItem key={i}>
-                    <Text value={item.name}/>
-                    <FiveChecks value={item.value} />
+                    <Text name={`${name}.${i}.0`}/>
+                    <FiveChecks name={`${name}.${i}.1`} />
                 </ListItem>
             ))}
             <AddButton onClick={addNew} />
