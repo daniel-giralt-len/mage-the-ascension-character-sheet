0c3bd5bfd64581226f3d234be09ff46672b6533e
daniel-giralt-len <daniel.giralt.len@gmail.com>
2023-04-30 19:21:24 +0200

    use react-hook-form for counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,3 +1,9 @@
-export const Counter = () => (
-    <input type='number' min={0}/>
-)
+import { useFormContext } from "react-hook-form"
+import { CounterProps } from "./types"
+
+export const Counter: React.FC<CounterProps> = ({ name }) => {
+    const { register } = useFormContext()
+    return (<input type='number' min={0}
+        {...register(name, {valueAsNumber: true})}
+    />)
+}
