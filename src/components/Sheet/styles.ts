d17045b8f14d5264733d882698e4cbdf602e74f4
daniel-giralt-len <daniel.giralt.len@gmail.com>
2023-05-26 19:59:23 +0200

    make font bigger

--- src/components/Sheet/styles.ts
+++ src/components/Sheet/styles.ts
@@ -3,5 +3,6 @@ import {createGlobalStyle} from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
     body {
         font-family: 'Josefin Sans', sans-serif;
+        font-size: 1.25em;
     }
 `;
